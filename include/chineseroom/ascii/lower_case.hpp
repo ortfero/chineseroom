13fcf787233e386283a0900baef3f8a3128cfc9f
Andrei Ilin <ortfero@gmail.com>
2020-07-21 14:19:04 +0300

    Add ascii::lower_case

---
+++ include/chineseroom/ascii/lower_case.hpp
@@ -0,0 +1,65 @@
+/* This file is part of chineseroom library
+ * Copyright 2020 Andrei Ilin <ortfero@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+
+#include "table.hpp"
+
+
+namespace chineseroom::ascii {
+  
+  
+  template<typename S>
+  S& lower_case(S& string) {
+    
+    static_assert(sizeof(typename S::value_type) == 1,
+      "Only char based strings are supported");
+      
+    if(string.empty())
+      return string;
+      
+    char* p = string.data();
+    for(;;)
+      switch(*p) {
+        case 'A': case 'B': case 'C': case 'D':
+        case 'E': case 'F': case 'G': case 'H':
+        case 'I': case 'J': case 'K': case 'L':
+        case 'M': case 'N': case 'O': case 'P':
+        case 'Q': case 'R': case 'S': case 'T':
+        case 'U': case 'V': case 'W': case 'X':
+        case 'Y': case 'Z':
+          *p = *p - 'A' + 'a';
+          ++p;
+          continue;
+        case '\0':
+          return string;
+        default:
+          ++p;
+          continue;
+      }
+      
+    
+  }
+  
+  
+}
